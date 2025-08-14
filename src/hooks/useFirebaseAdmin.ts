af5ccce78b122ed0eef161c7d36c53ad7f3b9c64
Marjon Godito <marjon.godito@gmanmi.com>
2025-08-14 20:10:02 +0800

    update: delete firebase.json

--- src/hooks/useFirebaseAdmin.ts
+++ src/hooks/useFirebaseAdmin.ts
@@ -4,7 +4,23 @@ import { getFirestore } from 'firebase-admin/firestore';
 import { getAuth } from 'firebase-admin/auth';
 import 'server-only';
 
-import account from '@/keys/firebase.json';
+function env(key : string){
+    return process.env[key]
+}
+
+const account = {
+    type: env('TYPE'),
+    project_id: env('PROJECT_ID'),
+    private_key_id: env('PRIVATE_KEY_ID'),
+    private_key: env('PRIVATE_KEY'),
+    client_email: env('CLIENT_EMAIL'),
+    client_id: env('CLIENT_ID'),
+    auth_uri: env('AUTH_URI'),
+    token_uri: env('TOKEN_URI'),
+    auth_provider_x509_cert_url: env('AUTH_PROVIDER_X509_CERT_URL'),
+    client_x509_cert_url: env('CLIENT_X509_CERT_URL'),
+    universe_domain: env('UNIVERSE_DOMAIN')
+}
 
 export class Firebase {
 
